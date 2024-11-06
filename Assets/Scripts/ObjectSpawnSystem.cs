6f6b5f48207064db88cfc02f7ccdf944ee68ea4f
Rees Amarteifio <reesamarteifio@gmail.com>
2024-11-06 13:49:47 -0600

    Single object spawn system

--- Assets/Scripts/ObjectSpawnSystem.cs
+++ Assets/Scripts/ObjectSpawnSystem.cs
@@ -1,7 +1,9 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -15,26 +17,47 @@ public class ObjectSpawnSystem : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] GameObject element;
+    [SerializeField] GameObject player;
+    [SerializeField] Vector3 playerInitialPosition;
+    [SerializeField] GameObject virtualCamera;
     [SerializeField] ElementVariation elementVariation;
+    bool popBack = false;
 
     [Serializable]
-    struct ElementVariation 
+    struct ElementVariation
     {
         public int numberOfObjects;
-        public float popBackDistance;
-        public float colorSeed;
+        [Header("Pop Back Settings")]
+        public float popBackProximity;
+        public int popBackAtLast;
+        [NonSerialized] public Vector3 popBackAtLastPosition;
+        [Header("Wave \"Randomization\" Properties")]
+        public float phaseSeed;
+        public float randomizationCycleFrequency;
+        [Header("Color Properties")]
+        public bool overwriteColor;
+        [Header("Transform Properties")]
         public Vector3 spawnSpacing;
+        public Vector3 position;
+        public Vector3 rotation;
         public Vector3 scale;
+        
 
         float rand(float seed, float index, float numberOfObjects) 
         {
-            return ((seed + (seed * Mathf.Pow(2, index))) % numberOfObjects) / numberOfObjects;
+            float frequency = randomizationCycleFrequency;
+            float divisor = numberOfObjects != 0 ? numberOfObjects : 1;
+            float waveValue = Mathf.Abs(Mathf.Sin(Mathf.PI * frequency * (index / divisor)) + 
+                Mathf.Sin((Mathf.PI * frequency * index) + seed))
+                % 1;
+            //return ((seed + (seed * Mathf.Pow(2, index))) % numberOfObjects) / numberOfObjects;
+            return waveValue;
         }
         public Vector4 ColorRandomization(float seed, float index, float numberOfObjects)
         {
             return new Vector4(rand(seed, index, numberOfObjects), 
-                rand(seed, index + 1, numberOfObjects), 
-                rand(seed, index + 2, numberOfObjects), 1);
+                rand(seed, index + seed, numberOfObjects), 
+                rand(seed, index + seed * 2, numberOfObjects), 1);
         }
         public override int GetHashCode()
         {
@@ -44,57 +67,76 @@ public class ObjectSpawnSystem : MonoBehaviour
 
     IEnumerator SpawnObjects()
     {
-        List<GameObject> objectList = new List<GameObject>();
+       
         while (true)
         {
-            if (transform.childCount != 0)
+            if (popBack)
+            {
+                elementVariation.phaseSeed += Mathf.PI;
+            }
+            if (transform.childCount != 0 || popBack)
             {
                 int nDeleted = 0;
                 int childCount = transform.childCount;
-                print($"Initial child count: {childCount}");
-                int deleteAtIndex = 0;
-                while(transform.childCount > deleteAtIndex)
+                
+                while(transform.childCount > 0)
                 {
-                    Transform child = transform.GetChild(deleteAtIndex);
-                    if (child.GetComponent<ObjectSpawnSystem>() != null) 
-                    {
-                        deleteAtIndex += 1;
-                        continue;
-                    }
+                    Transform child = transform.GetChild(0);
                     child.parent = null;
                     DestroyImmediate(child.gameObject);
                     nDeleted += 1;
                 }
                 
-                print($"nDeleted {nDeleted} childCount {transform.childCount}");
-                    //objectList = new List<GameObject>();
             }
+            //spawn objects
             for (int i = 0; i < elementVariation.numberOfObjects; i++)
             {
                 GameObject g = Instantiate(element, transform);
                 g.name = gameObject.name + "_" + i;
                 g.transform.localScale = elementVariation.scale;
-                g.transform.position = transform.TransformPoint(elementVariation.spawnSpacing * i);
-                Vector4 color = elementVariation.ColorRandomization(elementVariation.colorSeed, i, elementVariation.numberOfObjects);
+                g.transform.position = transform.TransformPoint(elementVariation.spawnSpacing * i) + elementVariation.position;
+                if (player != null && i == elementVariation.numberOfObjects - elementVariation.popBackAtLast) 
+                {
+                    elementVariation.popBackAtLastPosition = g.transform.position;
+                }
+                g.transform.eulerAngles = transform.TransformVector(elementVariation.rotation);
+                Vector4 color = elementVariation.ColorRandomization(elementVariation.phaseSeed, i, elementVariation.numberOfObjects);
                 //print($"Color of house {i} = {color}");
-                g.GetComponent<SpriteRenderer>().color = color;
-                
+                if (elementVariation.overwriteColor)
+                {
+                    g.GetComponent<SpriteRenderer>().color = color;
+                }
             }
             ElementVariation oldElementVariation = elementVariation;
-            yield return new WaitUntil(() => !elementVariation.Equals(oldElementVariation));
+            yield return new WaitUntil(() => popBack || !elementVariation.Equals(oldElementVariation));
         }
     }
 
     [ExecuteInEditMode]
     void Start()
     {
+        if (player != null)
+        {
+            playerInitialPosition = player.transform.position;
+        }
         StartCoroutine(SpawnObjects());
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        popBack = false;
+        if (player != null && elementVariation.popBackAtLastPosition.x - player.transform.position.x
+            <= elementVariation.popBackProximity) //then pop back
+        {
+            popBack = true;
+            Vector3 velocity = player.GetComponent<Rigidbody2D>().velocity;
+            player.GetComponent<FlightControl>().enabled = false;
+            Vector3 newPosition = player.transform.position;
+            newPosition.x = playerInitialPosition.x;
+            player.transform.position = newPosition;
+            player.GetComponent<Rigidbody2D>().velocity = velocity;
+            player.GetComponent<FlightControl>().enabled = true;
+        }
     }
 }
