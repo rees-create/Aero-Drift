721a17c7c8329a537c4368c94b3a782d437c39a4
Rees Amarteifio <reesamarteifio@gmail.com>
2024-11-14 10:42:54 -0600

    Seems like it's not optimal to make the spawn system animate objects, so I just implemented a more capable feature randomization system.

--- Assets/Scripts/ObjectSpawnSystem.cs
+++ Assets/Scripts/ObjectSpawnSystem.cs
@@ -30,12 +30,15 @@ public class ObjectSpawnSystem : MonoBehaviour
         [Header("Pop Back Settings")]
         public float popBackProximity;
         public int popBackAtLast;
+        //[Header("Animation")]
+        //public Animator animator;
+        //public bool animateOnTrigger;
+        //public TransformAnimation transformAnimations;
         [NonSerialized] public Vector3 popBackAtLastPosition;
         [Header("Wave \"Randomization\" Properties")]
         public float phaseSeed;
         public float randomizationCycleFrequency;
-        [Header("Color Properties")]
-        public bool overwriteColor;
+        public Randomizable overwriteProperties;
         [Header("Transform Properties")]
         public Vector3 spawnSpacing;
         public Vector3 position;
@@ -53,16 +56,50 @@ public class ObjectSpawnSystem : MonoBehaviour
             //return ((seed + (seed * Mathf.Pow(2, index))) % numberOfObjects) / numberOfObjects;
             return waveValue;
         }
-        public Vector4 ColorRandomization(float seed, float index, float numberOfObjects)
+        public Vector4 RandomVector4(float seed, float index, float numberOfObjects)
         {
             return new Vector4(rand(seed, index, numberOfObjects), 
                 rand(seed, index + seed, numberOfObjects), 
                 rand(seed, index + seed * 2, numberOfObjects), 1);
         }
+        public Vector3 RandomVector3(float seed, float index, float numberOfObjects)
+        {
+            return new Vector3(rand(seed, index, numberOfObjects),
+                rand(seed, index + seed, numberOfObjects),
+                rand(seed, index + seed * 2, numberOfObjects)
+                );
+        }
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
+    }
+    //[Serializable]
+    //public struct TransformAnimation 
+    //{
+
+    //}
+
+    [Serializable]
+    public enum OverwriteType { This, Object, Both }
+    [Serializable]
+    struct Randomizable
+    {
+        
+        public OverwriteType type;
+        public bool spawnSpacing;
+        public bool color;
+        public bool position;
+        public bool rotation;
+        public bool scale;
+    }
+    Vector3 multiplyVectors(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+    }
+    Vector3 divideVectors(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
     }
 
     IEnumerator SpawnObjects()
@@ -91,21 +128,76 @@ public class ObjectSpawnSystem : MonoBehaviour
             //spawn objects
             for (int i = 0; i < elementVariation.numberOfObjects; i++)
             {
+                //initialize overwrite conditions
+                bool overwriteBoth = elementVariation.overwriteProperties.type == OverwriteType.Both;
+                bool overwriteThis = elementVariation.overwriteProperties.type == OverwriteType.This || overwriteBoth;
+                bool overwriteObject = elementVariation.overwriteProperties.type == OverwriteType.Object || overwriteBoth;
+                //instantiate and get to work
                 GameObject g = Instantiate(element, transform);
+                //check and change overwrite type of object
+                if (overwriteObject)
+                {
+                    OverwriteType type = g.GetComponent<ObjectSpawnSystem>().elementVariation.overwriteProperties.type;
+                    if (type == OverwriteType.Object && g.GetComponent<ObjectSpawnSystem>() != null) //make object randomizable
+                    {
+                        g.GetComponent<ObjectSpawnSystem>().elementVariation.overwriteProperties.type = OverwriteType.Both;
+                    }
+                    else if(g.GetComponent<ObjectSpawnSystem>() != null) 
+                    {
+                        print("Your object doesn't have an ObjectSpawnSystem");
+                    }
+                }
+               
+                //set name
                 g.name = gameObject.name + "_" + i;
-                g.transform.localScale = elementVariation.scale;
-                g.transform.position = transform.TransformPoint(elementVariation.spawnSpacing * i) + elementVariation.position;
+                //get scale
+                Vector3 scale = elementVariation.scale;
+                //potentially randomize scale
+                if (elementVariation.overwriteProperties.scale && overwriteThis)
+                {
+                    scale = multiplyVectors(elementVariation.scale, elementVariation.RandomVector3(elementVariation.phaseSeed, i, elementVariation.numberOfObjects));
+                }
+                //set scale
+                g.transform.localScale = scale;
+                //get position
+                Vector3 position = elementVariation.position;
+                //potentially randomize position
+                if (elementVariation.overwriteProperties.position && overwriteThis)
+                {
+                    position = multiplyVectors(position, elementVariation.RandomVector3(elementVariation.phaseSeed, i, elementVariation.numberOfObjects));
+                }
+                //potentially randomize spawnSpacing
+                Vector3 spawnSpacing = elementVariation.spawnSpacing;
+                if (elementVariation.overwriteProperties.spawnSpacing && overwriteThis)
+                {
+                    spawnSpacing = multiplyVectors(spawnSpacing, elementVariation.RandomVector3(elementVariation.phaseSeed, i, elementVariation.numberOfObjects));
+                }
+                //set position
+                g.transform.position = transform.TransformPoint(spawnSpacing * i) + position;
+                //set pop back position
                 if (player != null && i == elementVariation.numberOfObjects - elementVariation.popBackAtLast) 
                 {
                     elementVariation.popBackAtLastPosition = g.transform.position;
                 }
-                g.transform.eulerAngles = transform.TransformVector(elementVariation.rotation);
-                Vector4 color = elementVariation.ColorRandomization(elementVariation.phaseSeed, i, elementVariation.numberOfObjects);
-                //print($"Color of house {i} = {color}");
-                if (elementVariation.overwriteColor)
+                //get rotation
+                Vector3 rotation = transform.TransformVector(elementVariation.rotation);
+                //potentially randomize rotation
+                if (elementVariation.overwriteProperties.rotation && overwriteThis)
+                {
+                    rotation += multiplyVectors(rotation, elementVariation.RandomVector3(elementVariation.phaseSeed, i, elementVariation.numberOfObjects));
+                }
+                //set rotation
+                g.transform.eulerAngles = rotation;
+                
+                //calculate potentially random color
+                Vector4 color = elementVariation.RandomVector4(elementVariation.phaseSeed, i, elementVariation.numberOfObjects);
+                //potentially set random color
+                if (elementVariation.overwriteProperties.color && overwriteThis)
                 {
                     g.GetComponent<SpriteRenderer>().color = color;
                 }
+                
+                
             }
             ElementVariation oldElementVariation = elementVariation;
             yield return new WaitUntil(() => popBack || !elementVariation.Equals(oldElementVariation));
